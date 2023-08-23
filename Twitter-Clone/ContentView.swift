569de2b10e9be3fd4e2663fcc61c380cea487c56
Alejandro D <ajdiaz5800@gmail.com>
2023-08-23 16:35:03 -0700

    Initial Commit

---
+++ Twitter-Clone/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Twitter-Clone
+//
+//  Created by Alex Diaz on 8/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Twitter-Clone/Twitter_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_CloneApp.swift
+//  Twitter-Clone
+//
+//  Created by Alex Diaz on 8/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
